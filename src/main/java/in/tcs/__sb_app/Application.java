ab5ef9958e62ca94e8c8abec4193ccb7e939423f
prem6303 <preamreddy64257@gmail.com>
2025-03-25 09:16:49 +0530

    Update Application.java

--- src/main/java/in/tcs/__sb_app/Application.java
+++ src/main/java/in/tcs/__sb_app/Application.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		int i =10;
+		int i = 10;
 		SpringApplication.run(Application.class, args);
-		int j =20;
+		int j = 20;
 		System.out.println(i);
 		int k = 30;
 		System.out.println(k);
